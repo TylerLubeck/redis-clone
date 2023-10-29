2cd9db7609750ca1b68543d24aa368d1eb33a261
Tyler Lubeck <tyler@tylerlubeck.com>
2023-10-29 17:22:44 -0600

    Workign pong?

--- app/server.go
+++ app/server.go
@@ -2,10 +2,41 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	// Uncomment this block to pass the first stage
+	"io"
 	 "net"
 	 "os"
+	 "sync"
 )
+
+const (
+	NUM_CONNECTIONS = 200
+	NUM_WORKERS = 50
+)
+
+
+func handleCommand(command, payload string) string {
+	switch command {
+	case "+PING":
+		return "PONG"
+	default:
+		return "WTF"
+	}
+}
+
+
+func worker(wg *sync.WaitGroup, connections <-chan net.Conn) {
+	defer wg.Done()
+	for conn := range connections {
+		fmt.Println("Handling connection")
+		//io.Copy(conn, conn)
+		//io.Copy(conn, strings.NewReader("+PONG\r\n"))
+		io.Copy(conn, strings.NewReader("+" + handleCommand("+PING", "") +"\r\n"))
+		conn.Close()
+	}
+}
+
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -18,9 +49,25 @@ func main() {
 	 	fmt.Println("Failed to bind to port 6379")
 	 	os.Exit(1)
 	 }
-	 _, err = l.Accept()
-	 if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
+	 fmt.Printf("Listening on %v", l.Addr())
+	 defer l.Close()
+
+	 connections := make(chan net.Conn, NUM_CONNECTIONS)
+
+	 var wg sync.WaitGroup
+
+	 for i := 0; i < NUM_WORKERS; i++ {
+		wg.Add(1)
+		go worker(&wg, connections)
+	 }
+
+	 for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Printf("Error accepting connection: %v\n", err)
+			os.Exit(1)
+		}
+
+		connections <- conn
 	 }
 }
